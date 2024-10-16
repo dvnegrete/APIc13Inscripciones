5d9b0919bd429aef8b27de98200e2c001bd92cf1
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2024-10-16 14:03:39 -0600

    fix: metodo en CORS enviado a FRONT y reintentos a BD en hibernacion

--- src/middlewares/wakeUpDB.js
+++ src/middlewares/wakeUpDB.js
@@ -2,8 +2,8 @@ import { serverUnavailable } from "@hapi/boom";
 import { database } from "../database/mysql.js";
 
 export const wakeUpDB = async (req, res, next) => {
-  const maxRetries = 2;
-  const retryDelay = 1000;
+  const maxRetries = 3;
+  const retryDelay = 2000;
   const tryConnect = async (attempt = 1) => {
     try {
       await database.query("SELECT 1");
@@ -11,10 +11,9 @@ export const wakeUpDB = async (req, res, next) => {
     } catch (error) {
       console.warn("DB inactive, WAKE... attemp", attempt);
       if (attempt < maxRetries) {
-        setTimeout( async() =>await tryConnect(attempt + 1), retryDelay);
+        setTimeout(async () => await tryConnect(attempt + 1), retryDelay);
       } else {
-        return next(error);
-        // return next(serverUnavailable());
+        return next(serverUnavailable());
       }
     }
   };
